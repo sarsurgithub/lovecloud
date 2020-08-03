f0213787558a265600bb8721a80b9b629c26493f
sarsurgithub <sarah.jallon@gmail.com>
2020-08-03 21:56:56 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <page/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import page from '@/components/page.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    page,
   },
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica Neue, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+    background-color: #192974;
+    margin: 0px;
+
 }
 </style>
---
+++ src/components/page.vue
@@ -0,0 +1,294 @@
+<template>
+  <div class='page'>
+    <section class="one">
+      <div class='one-inside'>
+        <div class='textone'>
+          <h1>Le Cloud Computing</h1>
+          <h2>Qu'est-ce que c'est ?</h2>
+          <p> blabla
+            blabla
+          </p>
+        </div>
+        <img class='cloud' src='@/assets/pictures/supercloud.png'/>
+      </div>
+    </section>
+    <section class="two">
+      <div class='two-inside'>
+        <img class='heig' src='@/assets/pictures/heigorange.png'/>
+        <div class='texttwo'>
+          <h1>Comment se former</h1>
+          <h2>aux technologies du web ?</h2>
+          <p> blabla </p>
+        </div>
+      </div>
+    </section>
+    <div class=wrap>
+      <section class='minisection'/>
+    </div>
+    <section class="three">
+      <div class='three-inside'>
+        <div class='textthree'>
+          <h1>Avenir</h1>
+          <h2>Professionnel</h2>
+          <p> blabla </p>
+        </div>
+        <img class='' src=''/>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+  .page {
+    color: lightgray;
+  }
+
+    /* Extra small devices (phones, 600px and down) */
+  @media only screen and (max-width: 600px) {
+    h1 {
+      font-size: 36px;
+      margin-right: 5%;
+
+    }
+    h2 {
+      font-size: 24px;
+      margin-left: 20%;
+
+    }
+    p {
+      font-size: 16px;
+      padding-left:50px;
+      max-width: 500px;
+      text-align: left;
+    }
+    section {
+    padding: 30vh 0;
+  }
+  .one {
+    padding-top: 15%;
+    position: relative;
+    z-index: 12;
+    margin-bottom: -35vw;
+    clip-path: polygon(0 0, 100% 0, 100% calc(100% - 35vw), 0% 100%);
+    background-color: #192974;
+
+    .one-inside {
+      padding-bottom: 30%;
+
+      .textone {
+        h1 {
+          color:white;
+          margin: 10px;
+          margin-right: 5%;
+        }
+        h2 {
+          font-weight: normal;
+          color: #65BAEF;
+          margin: 10px;
+        }
+        p {
+          color: #D3DCE4;
+        }
+      }
+
+      .cloud {
+        width: 300px;
+      }
+    }
+  }
+
+  .two {
+    position: relative;
+    z-index: 11;
+    background-color: #F4F5FF;
+    .two-inside {
+
+      .texttwo {
+        padding-top: 5%;
+        h1 {
+          color:#192974;
+          margin: 10px;
+          margin-right: 10%;
+        }
+        h2 {
+          font-weight: normal;
+          color: #65BAEF;
+          margin: 10px;
+          margin-left: 25%;
+        }
+        p {
+          color: #5C6975;
+        }
+      }
+    .heig {
+      width: 300px;
+      padding-top:20%;
+    }
+    }
+  }
+  .minisection {
+    padding: 18vh 0;
+    position: relative;
+    z-index: 12;
+    clip-path: polygon(0 0, 100% calc(0% + 10vw), 100% 100%, 0 100%);
+    margin-top: -10vw;
+    background-color: #1C90E6;
+  }
+  .three{
+    position: relative;
+    z-index: 13;
+    clip-path: polygon(0 0, 100% calc(0% + 10vw), 100% 100%, 0 100%);
+    margin-top: -10vw;
+    background-color: #192974;
+    .three-inside {
+      padding-bottom: 30%;
+
+      .textthree {
+        h1 {
+          color:white;
+          margin: 10px;
+          margin-right: 10%;
+        }
+        h2 {
+          font-weight: normal;
+          color: #65BAEF;
+          margin: 10px;
+          margin-left: 40%;
+        }
+        p {
+          color: #D3DCE4;
+        }
+      }
+    }
+  }
+  }
+
+  @media only screen and (min-width: 1200px){
+  section {
+    padding: 30vh 0;
+  }
+  .one {
+    padding-top: 15%;
+    position: relative;
+    z-index: 12;
+    margin-bottom: -35vw;
+    clip-path: polygon(0 0, 100% 0, 100% calc(100% - 35vw), 0% 100%);
+    background-color: #192974;
+
+    .one-inside {
+      display: flex;
+      justify-content: space-evenly;
+      padding-bottom: 30%;
+
+      .textone {
+        h1 {
+          font-size: 40px;
+          color:white;
+          margin: 10px;
+          margin-right: 5%;
+        }
+        h2 {
+          font-size: 32px;
+          font-weight: normal;
+          color: #65BAEF;
+          margin: 10px;
+          margin-left: 20%;
+        }
+        p {
+          color: #D3DCE4;
+          width: 500px;
+          text-align: left;
+          padding-left:50px;
+          font-size: 24px;
+        }
+      }
+
+      .cloud {
+        width: 700px;
+      }
+    }
+  }
+
+  .two {
+    position: relative;
+    z-index: 11;
+    background-color: #F4F5FF;
+    .two-inside {
+      display: flex;
+      justify-content: space-evenly;
+      .texttwo {
+        padding-top: 5%;
+        h1 {
+          font-size: 40px;
+          color:#192974;
+          margin: 10px;
+          margin-right: 10%;
+        }
+        h2 {
+          font-size: 32px;
+          font-weight: normal;
+          color: #65BAEF;
+          margin: 10px;
+          margin-left: 25%;
+        }
+        p {
+          color: #5C6975;
+          width: 500px;
+          text-align: left;
+          padding-left:50px;
+          font-size: 24px;
+        }
+      }
+    .heig {
+      width: 700px;
+      padding-top:20%;
+    }
+    }
+  }
+  .minisection {
+    padding: 18vh 0;
+    position: relative;
+    z-index: 12;
+    clip-path: polygon(0 0, 100% calc(0% + 10vw), 100% 100%, 0 100%);
+    margin-top: -10vw;
+    background-color: #1C90E6;
+  }
+  .three{
+    position: relative;
+    z-index: 13;
+    clip-path: polygon(0 0, 100% calc(0% + 10vw), 100% 100%, 0 100%);
+    margin-top: -10vw;
+    background-color: #192974;
+    .three-inside {
+      display: flex;
+      justify-content: space-evenly;
+      padding-bottom: 30%;
+
+      .textthree {
+        h1 {
+          font-size: 40px;
+          color:white;
+          margin: 10px;
+          margin-right: 10%;
+        }
+        h2 {
+          font-size: 32px;
+          font-weight: normal;
+          color: #65BAEF;
+          margin: 10px;
+          margin-left: 40%;
+        }
+        p {
+          color: #D3DCE4;
+          width: 500px;
+          text-align: left;
+          padding-left:50px;
+          font-size: 24px;
+        }
+      }
+    }
+  }
+}
+
+</style>
